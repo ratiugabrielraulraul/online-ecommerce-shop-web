b728b22fd57e59b39f20480c1fc4a489b448f406
Raul <ratiugabrielraul@gmail.com>
2019-11-06 22:04:27 +0200

    added  cart remove item functionality

--- js/cart.js
+++ js/cart.js
@@ -17,15 +17,15 @@ window.Cart = {
         var allProductsHtml = "";
 
         products.forEach(product => allProductsHtml += Cart.getProductHtml(product));
-        allProductsHtml += Cart.addcheckout();
+        allProductsHtml += Cart.addcheckout(5);
         $(".shop_table.cart tbody").html(allProductsHtml);
     },
 
 
     getProductHtml: function (product) {
-        return `<tr class="cart_item id-${product.id}">
+        return `<tr class="cart_item id-${product.id} ${product.id}">
                                             <td class="product-remove">
-                                                <a title="Remove this item" class="remove" data-product_id="${product.id}" onclick="" href="#">×</a> 
+                                                <a title="Remove this item" class="remove" data-product_id="${product.id}" onclick="Cart.deleteProduct(${product.id}); return false;" href="#">×</a> 
                                             </td>
 
                                             <td class="product-thumbnail">
@@ -43,7 +43,7 @@ window.Cart = {
                                             <td class="product-quantity-${product.id}">
                                                 <div class="quantity buttons_added">
                                                     <input type="button" class="minus" value="-" onclick="Cart.addMinusButton(${product.id}); return false;">
-                                                    <input type="number" size="4" class="input-text qty text" title="Qty" value="1" min="0" step="1">
+                                                    <input type="number" size="4" class="input-text qty${product.id} text" title="Qty" value="${product.quantity}" min="0" step="1">
                                                     <input type="button" class="plus" value="+" onclick="Cart.addPlusButton(${product.id}); return false;">
                                                 </div>
                                             </td>
@@ -63,21 +63,32 @@ window.Cart = {
             nextValue = parseInt(currentValue - 1);
         $(`.product-quantity-${id}`).find('input.input-text').val(nextValue);
     },
-
     deleteProduct: function (productId) {
+        console.log(productId);
         $.ajax({
-            url:Cart.API_BASE_URL + "/carts/" + 15,
+            url:Cart.API_BASE_URL + "/carts/remove/15/" + productId,
             method:"DELETE"
         }).done(function (response) {
             console.log(response);
-            Cart.displayProducts(response.products);
+            $(`.${productId}`).html('');
+            // Cart.displayProducts(response.products);
         })
-
+    },
+    updateProductCount: function(productId) {
+        var count = $(`.qty${productId}`).val();
+        $.ajax({
+            url:Cart.API_BASE_URL + "/carts/update/count/15/" + productId + "/" + count,
+            method:"PUT"
+        }).done(function (response) {
+            console.log(response);
+            // $(`.${productId}`).html('');
+            // Cart.displayProducts(response.products);
+        })
     },
 
 
 
-    addcheckout: function () {
+    addcheckout: function (productId) {
         return ` <tr>
                                             <td class="actions" colspan="6">
                                                 <div class="coupon">
@@ -85,7 +96,7 @@ window.Cart = {
                                                     <input type="text" placeholder="Coupon code" value="" id="coupon_code" class="input-text" name="coupon_code">
                                                     <input type="submit" value="Apply Coupon" name="apply_coupon" class="button">
                                                 </div>
-                                                <input type="submit" value="Update Cart" name="update_cart" class="button">
+                                                <input type="submit" onclick="Cart.updateProductCount(${productId}); return false;" value="Update Cart" name="update_cart" class="button">
                                                 <input type="submit" value="Proceed to Checkout" name="proceed" class="checkout-button button alt wc-forward">
                                             </td>
                                         </tr>`;
